826cb5643d0a3b77ef8b4b6d53f6f73d394ee868
Pavel Ganzhela <p.ganzhela@omprussia.ru>
2020-10-22 20:01:34 +0300

    [new] Added test function

--- main.c
+++ main.c
@@ -25,11 +25,33 @@ float division(float a, float b){
 	}
 }
 
+void test(void){
+	printf("Результат сложения 4 и 2: %d", sum(4,2));
+	if(sum(4,2) == 6) printf(" - правильно\n");
+	else printf(" - не правильно\n");
+
+	printf("Результат умножения 4 и 2: %d", multiply(4,2));
+	if(multiply(4,2) == 8) printf(" - правильно\n");
+	else printf(" - не правильно\n");
+
+	printf("Результат вычитания 4 и 2: %d", subtraction(4,2));
+	if(subtraction(4,2) == 2) printf(" - правильно\n");
+	else printf(" - не правильно\n");
+
+	printf("Результат деления 4 и 2: %f", division(4,2));
+	if(division(4,2) == 2) printf(" - правильно\n");
+	else printf(" - не правильно\n");
+
+	printf("Результат деления 4 и 0: %f", division(4,0));
+	if(division(4,0) == -1) printf(" - правильно\n");
+	else printf(" - не правильно\n");
+}
+
 int main(int argc, char const *argv[])
 {
 	int rez = 0;
 	rez = printf("Hello, World!\n");
-	
+	test();
 	// проверка на ошибку функции printf
 	if(rez < 0) return -1;
 	return 0;
